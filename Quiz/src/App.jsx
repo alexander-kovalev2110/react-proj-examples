5ceb2f01abe2b463893939abe72d93117c9fff6a
alexander-kovalev2110 <alexander.kovalev@yahoo.com>
2025-05-01 15:23:10 +0300

    QuizUpd

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -23,11 +23,11 @@ const questions = [
   },
 ];
 
-function Result({ correct }) {
+const Result = ({ correct, length }) => {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>You guessed {correct} answers out of {questions.length} </h2>
+      <h2>You guessed {correct} answers out of {length} </h2>
       <a href="/">
         <button >Try again</button>
       </a>
@@ -35,15 +35,11 @@ function Result({ correct }) {
   );
 }
 
-function Game({ step, question, onClickVariant }) {
+const Game = ({ question, onClickVariant }) => {
   const { title, variants } = question;
-  const percentage = Math.round((step / questions.length) * 100);
 
   return (
     <>
-      <div className="progress">
-        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-      </div>
       <h1>{title}</h1>
       <ul>
         { variants.map((text, index) => (
@@ -54,15 +50,13 @@ function Game({ step, question, onClickVariant }) {
   );
 }
 
-function App() {
+const App = () => {
 const [step, setStep] = useState(0);
 const [correct, setCorrect] = useState(0);
-
 const question = questions[step];
 
 const onClickVariant = (index) => {
   setStep(step + 1);
-
   if (index === question.correct) {
     setCorrect(correct + 1);
   }
@@ -71,9 +65,18 @@ const onClickVariant = (index) => {
   return (
     <div className="App">
       {step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant}/>
+        <>
+          {/* Progress bar */}
+          <div className="progress">
+            <div 
+              style={{ width: `${Math.round((step / questions.length) * 100)}%` }} 
+              className="progress__inner">
+            </div>
+          </div>
+          <Game question={question} onClickVariant={onClickVariant}/>
+        </>
       ) : (
-        <Result correct={correct}/>
+        <Result correct={correct} length={questions.length}/>
       )}
     </div>
   );
